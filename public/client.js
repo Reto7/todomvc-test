d96a507df457de40c6896b7d1ccdc50fec0d87bd
Reto Kaufmann <reto.kaufmann@adon.li>
2017-05-21 16:06:25 +0200

    init

---
+++ public/client.js
@@ -0,0 +1,44 @@
+//Das Script soll erst dann ausgeführt werden, soll die Website komplett geladen ist. Daher füge ich als erstes eine Rückruf-Funktion
+//ein, welche den nachfolgenden Script umschließt und dann ausgeführt wird, sobald die Website zu Ende geladen hat
+$(document).ready(function(){
+    // WebSocket
+    var socket = io.connect();
+    // neue Nachricht
+    socket.on('chat', function (data) {
+        var zeit = new Date(data.zeit);
+        $('#content').append(
+            $('<li></li>').append(
+                // Uhrzeit
+                $('<span>').text('[' +
+                                 (zeit.getHours() < 10 ? '0' + zeit.getHours() : zeit.getHours())
+                                 + ':' +
+                                 (zeit.getMinutes() < 10 ? '0' + zeit.getMinutes() : zeit.getMinutes())
+                                 + '] '
+                ),
+                // Name
+                $('<b>').text(typeof(data.name) != 'undefined' ? data.name + ': ' : ''),
+                // Text
+                $('<span>').text(data.text))
+        );
+        // nach unten scrollen
+        $('body').scrollTop($('body')[0].scrollHeight);
+    });
+    // Nachricht senden
+    function senden(){
+        // Eingabefelder auslesen
+        var name = $('#name').val();
+        var text = $('#text').val();
+        // Socket senden
+        socket.emit('chat', { name: name, text: text });
+        // Text-Eingabe leeren
+        $('#text').val('');
+    }
+    // bei einem Klick
+    $('#senden').click(senden);
+    // oder mit der Enter-Taste
+    $('#text').keypress(function (e) {
+        if (e.which == 13) {
+            senden();
+        }
+    });
+});
---
+++ server.js
@@ -0,0 +1,52 @@
+/**
+ * http://nodecode.de/chat-nodejs-websocket
+ * Created by rk on 21.05.17.
+ * m anschließend den Server zu starten, musst du den Befehl node server.js eingeben.
+ * Danach ist die Anwendung über die Adresse http://127.0.0.1:8080/ zu erreichen.
+ */
+var express = require('express')
+    ,   app = express()
+    ,   server = require('http').createServer(app)
+    ,   io = require('socket.io').listen(server)
+    ,   conf = require('./config.json');
+
+// Webserver
+// auf den Port x schalten
+server.listen(conf.port);
+
+// app.configure(function(){
+//     // statische Dateien ausliefern
+//     app.use(express.static(__dirname + '/public'));
+// });
+app.use(express.static(__dirname + '/public'));
+//
+
+// wenn der Pfad / aufgerufen wird
+app.get('/', function (req, res) {
+    // so wird die Datei index.html ausgegeben
+    res.sendfile(__dirname + '/public/index.html');
+});
+
+// URKA: wir merken uns den Client (unique id)
+let clientId = 0;
+
+// Websocket
+io.sockets.on('connection', function (socket) {
+    // der Client ist verbunden
+    //clientId++;
+
+    // URKA
+    // send the clients id to the client itself.
+    //socket.send(socket.id)
+
+    socket.emit('chat', { zeit: new Date(), text: 'Du bist nun mit dem Server verbunden! Deine Client Id ist ' +socket.id });
+
+    // wenn ein Benutzer einen Text senden
+    socket.on('chat', function (data) {
+        // so wird dieser Text an alle anderen Benutzer gesendet
+        io.sockets.emit('chat', { zeit: new Date(), name: data.name || 'Anonym', text: data.text + ", sent from client id "+socket.id});
+    });
+});
+
+// Portnummer in die Konsole schreiben
+console.log('Der Server läuft nun unter http://127.0.0.1:' + conf.port + '/');
